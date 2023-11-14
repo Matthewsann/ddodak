48676fe1b448002a4d2d61752a87ea4606009234
r4bb1t <r4bb1t.github@gmail.com>
2023-11-15 00:58:52 +0900

    style: button translate

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,11 @@ export default function Home() {
         </div>
       </div>
       <div className="w-full flex justify-center bg-background">
-        <button className="-translate-y-1/2 shadow-none btn btn-primary min-h-0 h-auto text-base py-4 px-[58px] font-extrabold rounded-full text-white">
-          상담사 추천 받기
-        </button>
+        <div className="-translate-y-1/2">
+          <button className="shadow-none btn btn-primary min-h-0 h-auto text-base py-4 px-[58px] font-extrabold rounded-full text-white">
+            상담사 추천 받기
+          </button>
+        </div>
       </div>
       <div className="w-full h-full bg-background">
         <section className="flex flex-col">
