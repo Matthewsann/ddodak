ac5223948e1e072053d42ed055f00796916bb65a
r4bb1t <r4bb1t.github@gmail.com>
2023-11-15 00:58:01 +0900

    style: scrolling  px

--- app/page.tsx
+++ app/page.tsx
@@ -26,16 +26,20 @@ export default function Home() {
         </button>
       </div>
       <div className="w-full h-full bg-background">
-        <section className="pl-7 flex flex-col gap-0.5">
-          <div className="font-medium text-sm">나랑 가까운 상담 찾기</div>
-          <div className="flex items-center text-xs font-light">
-            <Icons.Crosshair />
-            서울시 성북구
+        <section className="flex flex-col">
+          <div className="flex flex-col gap-0.5 px-7">
+            <div className="font-medium text-sm">나랑 가까운 상담 찾기</div>
+            <div className="flex items-center text-xs font-light">
+              <Icons.Crosshair />
+              서울시 성북구
+            </div>
           </div>
           <div className="mt-2 overflow-x-auto no-scrollbar">
-            <Suspense fallback={<CounselorListLoading />}>
-              <CounselorList />
-            </Suspense>
+            <div className="px-7 w-max">
+              <Suspense fallback={<CounselorListLoading />}>
+                <CounselorList />
+              </Suspense>
+            </div>
           </div>
         </section>
       </div>
