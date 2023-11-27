873a2fcfe36a071ab760a1348f8b9c0d9f6eceba
r4bb1t <r4bb1t.github@gmail.com>
2023-11-27 09:44:55 +0900

    fix: fix bottomsheet mount point

--- app/map/search-result.tsx
+++ app/map/search-result.tsx
@@ -1,24 +1,35 @@
 import Sheet from "react-modal-sheet";
 import CounselorCard from "../components/counselor/card";
+import { useRef } from "react";
 
 export default function SearchResult() {
+  const ref = useRef<HTMLDivElement>(null);
   return (
-    <Sheet
-      isOpen={true}
-      onClose={() => {}}
-      snapPoints={[800, 200]}
-      initialSnap={1}
+    <div
+      ref={ref}
+      className="absolute bottom-0 left-0 right-0 pointer-events-none h-full"
     >
-      <Sheet.Container className="!bg-background">
-        <Sheet.Header />
-        <Sheet.Content className="px-5 pb-12">
-          <Sheet.Scroller>
-            {[...Array(10)].map((_, i) => (
-              <CounselorCard key={i} />
-            ))}
-          </Sheet.Scroller>
-        </Sheet.Content>
-      </Sheet.Container>
-    </Sheet>
+      {ref.current && (
+        <Sheet
+          isOpen={true}
+          onClose={() => {}}
+          snapPoints={[800, 200]}
+          initialSnap={1}
+          mountPoint={ref.current}
+          className="!absolute"
+        >
+          <Sheet.Container className="!bg-background">
+            <Sheet.Header />
+            <Sheet.Content className="px-5 pb-12">
+              <Sheet.Scroller>
+                {[...Array(10)].map((_, i) => (
+                  <CounselorCard key={i} />
+                ))}
+              </Sheet.Scroller>
+            </Sheet.Content>
+          </Sheet.Container>
+        </Sheet>
+      )}
+    </div>
   );
 }
