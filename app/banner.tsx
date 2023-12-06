b55b2635c87ac3aac2bd5478e4472831b826be61
r4bb1t <r4bb1t.github@gmail.com>
2023-12-07 01:20:40 +0900

    feat: add swiper to banner

--- app/banner.tsx
+++ app/banner.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+
 export default function Banner() {
-  return <div className="w-full aspect-[375/105] bg-primary">banner</div>;
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      className="w-full aspect-[375/105]"
+      loop
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: true,
+      }}
+      modules={[Autoplay]}
+    >
+      <SwiperSlide className="w-full h-full bg-[url(https://images.unsplash.com/photo-1701657130003-fbdb40fd611a)] bg-cover bg-center"></SwiperSlide>
+      <SwiperSlide className="w-full h-full bg-[url(https://plus.unsplash.com/premium_photo-1701760275641-e60a89edad6a)] bg-cover bg-center"></SwiperSlide>
+    </Swiper>
+  );
 }
