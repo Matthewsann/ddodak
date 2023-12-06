86181e0ef76af37cbe8b4540745c83147761ecdd
r4bb1t <r4bb1t.github@gmail.com>
2023-12-07 00:02:07 +0900

    fix: 뒤로가기 버튼 누를 시 router back

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import Icons from "./icons";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <div className="w-full px-4 py-2">
-      <button className="btn btn-ghost btn-circle">
+      <button
+        className="btn btn-ghost btn-circle"
+        onClick={() => router.back()}
+      >
         <Icons.GoBack />
       </button>
     </div>
