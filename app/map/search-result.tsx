004f4937f140098124e732722bab02e8fc4ff5a4
r4bb1t <r4bb1t.github@gmail.com>
2023-12-27 16:32:21 +0900

    style: no-result

--- app/map/search-result.tsx
+++ app/map/search-result.tsx
@@ -70,8 +70,17 @@ export default function SearchResult({
                       />
                     ))
                 ) : (
-                  <div className="text-center text-gray-500 py-12">
-                    검색 결과가 없습니다.
+                  <div className="text-center flex flex-col items-center justify-center h-full">
+                    <div className="translate-y-4 font-medium text-sm">
+                      검색결과가 존재하지 않습니다.
+                    </div>
+                    <img
+                      src="/asset/map/no-result.png"
+                      className="w-[138px] h-[147px]"
+                    />
+                    <button className="btn btn-lg !rounded-full btn-primary w-full !text-white -translate-y-12">
+                      적용하기
+                    </button>
                   </div>
                 )}
               </Sheet.Scroller>
