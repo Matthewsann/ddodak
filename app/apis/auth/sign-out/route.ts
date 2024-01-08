027b9c3bb6f288c58cefb62fad05859d8a4c11fa
r4bb1t <r4bb1t.github@gmail.com>
2024-01-09 01:00:03 +0900

    fix: logout

--- app/apis/auth/sign-out/route.ts
+++ app/apis/auth/sign-out/route.ts
@@ -1,15 +1,16 @@
-const getCookie = (name: string) => {
-  return document.cookie.split(";").find((c) => {
+const getCookie = (cookies: string, name: string) => {
+  return cookies.split(";").find((c) => {
     return c.trim().startsWith(name);
   });
 };
 
-export const GET = async () => {
+export const GET = async (request: Request) => {
   const response = new Response();
 
   response.headers.set(
     "set-cookie",
     `token=${getCookie(
+      request.headers.get("cookie") || "",
       "SANGDAMPLUS"
     )}; Max-Age=0; Path=/; SameSite=None; Secure;`
   );
