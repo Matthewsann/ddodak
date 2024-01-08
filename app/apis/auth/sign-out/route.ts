4283b5bd25c68701b7d0243093d2b829838e2797
r4bb1t <r4bb1t.github@gmail.com>
2024-01-09 02:15:41 +0900

    fix: test

--- app/apis/auth/sign-out/route.ts
+++ app/apis/auth/sign-out/route.ts
@@ -4,10 +4,9 @@ export const GET = async (request: Request) => {
   const cookies = request.headers.get("Cookie");
   const cookie = cookies
     ?.split(";")
-    .find((cookie) => cookie.trim().startsWith("SANGDAMPLUS"))
-    ?.split("=")[0];
+    .find((cookie) => cookie.trim().startsWith("SANGDAMPLUS"));
 
-  console.log(cookie);
+  console.log(cookies);
 
   response.headers.set(
     "Set-Cookie",
