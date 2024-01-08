994b2f70941d7458d9ce2d76e621803ee6c81129
r4bb1t <r4bb1t.github@gmail.com>
2024-01-09 02:27:49 +0900

    fix: remove sign out (TODO)

--- app/apis/auth/sign-out/route.ts
+++ app/apis/auth/sign-out/route.ts
@@ -1,17 +1,7 @@
 export const GET = async (request: Request) => {
   const response = new Response();
 
-  const cookies = request.headers.get("cookie");
-  const cookie = cookies
-    ?.split(";")
-    .find((cookie) => cookie.trim().startsWith("SANGDAMPLUS"));
-
-  console.log(request.headers);
-
-  response.headers.set(
-    "Set-Cookie",
-    `${cookie}=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`
-  );
+  // TODO: sign out
 
   return response;
 };
